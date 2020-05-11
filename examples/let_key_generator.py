7bc1f53d7a0ad292607445e29c155a8f070bd8f8
ikonsty <i.konsty@gmail.com>
2020-05-11 12:55:28 +0300

    Add new structure

---
+++ examples/let_key_generator.py
@@ -0,0 +1,19 @@
+from random import randint
+from pprint import pprint
+
+alphabet = {}
+
+# dict_size = 128
+# dict = {chr(i): i for i in range(dict_size)}
+# pprint(dict)
+
+for i in range(65, 91):
+    rand_cord = '(' + str(randint(1, 10)) + ',' + str(randint(1, 10)) + ')'
+    while rand_cord in alphabet.values():
+        rand_cord = '(' + str(randint(1, 10)) + ',' + str(randint(1, 10)) + ')'
+    alphabet[chr(i)] = rand_cord
+
+f = open("letter_key.txt", "w+")
+for let in alphabet:
+    f.write("{} {}\n".format(let, alphabet[let]))
+f.close()
